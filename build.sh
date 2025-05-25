2252e7e1d2c0f4d57e118a8a0e37823f5ca142c9
sreejesht <sreejesh.t@gmail.com>
2025-05-25 19:23:02 +0530

    Initial project files

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Build and tag dev image
 docker build -t sreedocker911/react-app:dev .
+
+# Tag it as prod as well
+docker tag sreedocker911/react-app:dev sreedocker911/react-app:prod
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-docker run -d -p 80:80 --name react-app sreedocker911/react-app:dev
+
+# Default to dev if no argument passed
+target=${1:-dev}
+
+# Stop and remove existing container if running
+docker stop react-app 2>/dev/null || true
+docker rm react-app 2>/dev/null || true
+
+# Run specified image
+echo "Deploying sreedocker911/react-app:$target"
+docker run -d -p 80:80 --name react-app sreedocker911/react-app:$target
+
+# How to run
+# Deploy dev (default)
+#./deploy.sh
+
+# Deploy prod
+#./deploy.sh prod
