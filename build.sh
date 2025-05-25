2a215f8ccf6d0b2d2a8f49393b3105a8967bea85
sreejesht <sreejesh.t@gmail.com>
2025-05-26 02:03:34 +0530

    correction

--- build.sh
+++ build.sh
@@ -8,6 +8,11 @@ if [ ! -f package.json ]; then
   rm -rf temp-code
 fi
 
+# Confirm package.json is there
+if [ ! -f devops-build/package.json ]; then
+  echo "Error: package.json not found in devops-build/. Exiting."
+  exit 1
+fi
 # Build Docker image and tag for dev and prod
 docker build -t sreedocker911/react-app:dev .
 docker tag sreedocker911/react-app:dev sreedocker911/react-app:prod
